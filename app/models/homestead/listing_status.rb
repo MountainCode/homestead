bf971371f9515ba1499c736f9b99e1d182581b38
Chris Kentfield <kentfieldc@orvis.com>
2013-10-01 08:41:03 -0400

    Added foreign key to listing_status for listing

--- app/models/homestead/listing_status.rb
+++ app/models/homestead/listing_status.rb
@@ -2,6 +2,6 @@ module Homestead
   class ListingStatus < ActiveRecord::Base
     validates_presence_of :code
     validates_presence_of :name
-    has_many :listings
+    has_many :listings, foreign_key: :status_id
   end
 end
