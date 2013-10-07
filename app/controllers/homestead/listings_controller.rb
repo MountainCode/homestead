743f4d2b76d5b5f48f488a8f480c21964bbfeee5
Chris Kentfield <kentfieldc@orvis.com>
2013-10-07 09:36:41 -0400

    Added sorting by price

--- app/controllers/homestead/listings_controller.rb
+++ app/controllers/homestead/listings_controller.rb
@@ -2,7 +2,7 @@ module Homestead
   class ListingsController < ApplicationController
     def index
       # @listings = office.listings
-      @q = Listing.search(params[:q])
+      @q = Listing.search({'s' => 'list_price desc'}.merge(params[:q]))
       @listings = ListingDecorator.decorate_collection @q.result
     end
     def search
